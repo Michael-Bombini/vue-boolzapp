64b70b755e37649996abbdb7e72fb953e34a71a7
Michael Bombini <bombini.michael@gmail.com>
2022-06-15 11:38:47 +0200

    Successfully visualized filtered name

--- milestone4/main.js
+++ milestone4/main.js
@@ -1,10 +1,8 @@
 /*
-Milestone 3
-Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e 
-digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
-l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-
+Milestone 4
+ Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+“mar” rimangono solo Marco e Martina
 */
 
 const contatti = [
@@ -225,6 +223,16 @@ new Vue({
         this.newMessage = "";
         
       },
+
+      cercaContatto : function(){
+        const contattoInserito = this.searchingUser;
+        console.log(contattoInserito);
+        this.contatti.filter(contatto => {
+            if(contatto.name.includes(contattoInserito))
+              console.log(contatto.name);
+        });
+      }
+
 
 
     
