0b5b09055843866594abb70d801b81eb2f2ac7d7
Michael Bombini <bombini.michael@gmail.com>
2022-06-15 12:44:34 +0200

    Started working on point 2 of mile5 (adjusting 1)

--- milestone5/main.js
+++ milestone5/main.js
@@ -2,6 +2,7 @@
 Milestone 5 - opzionale
 Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
 permette di cancellare il messaggio selezionato
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 
 */
 
